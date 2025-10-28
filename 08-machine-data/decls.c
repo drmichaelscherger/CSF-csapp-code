e259a47cf58500d66cf5895b8b189b687a7aed08
Michael Scherger <m.scherger@tcu.edu>
2025-10-27 21:15:07 -0500

    Updated for Fall 2025

--- 08-machine-data/decls.c
+++ 08-machine-data/decls.c
@@ -3,10 +3,10 @@
 void nest1() {
     int A1[3];
     int *A2;
-    printf("A1 -> %d\n", sizeof(A1));
-    printf("A2 -> %d\n", sizeof(A2));
-    printf("*A1 -> %d\n", sizeof(*A1));
-    printf("*A2 -> %d\n", sizeof(*A2));
+    printf("A1 -> %ld\n", sizeof(A1));
+    printf("A2 -> %ld\n", sizeof(A2));
+    printf("*A1 -> %ld\n", sizeof(*A1));
+    printf("*A2 -> %ld\n", sizeof(*A2));
 }
 
 void nest2() {
@@ -15,18 +15,18 @@ void nest2() {
     int (*A3)[3];
     int (*A4[3]);
 
-    printf("A1 -> %d\n", sizeof(A1));
-    printf("A2 -> %d\n", sizeof(A2));
-    printf("A3 -> %d\n", sizeof(A3));
-    printf("A4 -> %d\n", sizeof(A4));
-    printf("*A1 -> %d\n", sizeof(*A1));
-    printf("*A2 -> %d\n", sizeof(*A2));
-    printf("*A3 -> %d\n", sizeof(*A3));
-    printf("*A4 -> %d\n", sizeof(*A4));
+    printf("A1 -> %ld\n", sizeof(A1));
+    printf("A2 -> %ld\n", sizeof(A2));
+    printf("A3 -> %ld\n", sizeof(A3));
+    printf("A4 -> %ld\n", sizeof(A4));
+    printf("*A1 -> %ld\n", sizeof(*A1));
+    printf("*A2 -> %ld\n", sizeof(*A2));
+    printf("*A3 -> %ld\n", sizeof(*A3));
+    printf("*A4 -> %ld\n", sizeof(*A4));
     //    printf("**A1 -> %d\n", sizeof(**A1));
-    printf("**A2 -> %d\n", sizeof(**A2));
-    printf("**A3 -> %d\n", sizeof(**A3));
-    printf("**A4 -> %d\n", sizeof(**A4));
+    printf("**A2 -> %ld\n", sizeof(**A2));
+    printf("**A3 -> %ld\n", sizeof(**A3));
+    printf("**A4 -> %ld\n", sizeof(**A4));
 }
 
 
@@ -37,26 +37,26 @@ void nest3() {
     int *(A4[3][5]);
     int (*A5[3])[5];
 
-    printf("A1 -> %d\n", sizeof(A1));
-    printf("A2 -> %d\n", sizeof(A2));
-    printf("A3 -> %d\n", sizeof(A3));
-    printf("A4 -> %d\n", sizeof(A4));
-    printf("A5 -> %d\n", sizeof(A5));
-    printf("*A1 -> %d\n", sizeof(*A1));
-    printf("*A2 -> %d\n", sizeof(*A2));
-    printf("*A3 -> %d\n", sizeof(*A3));
-    printf("*A4 -> %d\n", sizeof(*A4));
-    printf("*A5 -> %d\n", sizeof(*A5));
-    printf("**A1 -> %d\n", sizeof(**A1));
-    printf("**A2 -> %d\n", sizeof(**A2));
-    printf("**A3 -> %d\n", sizeof(**A3));
-    printf("**A4 -> %d\n", sizeof(**A4));
-    printf("**A5 -> %d\n", sizeof(**A5));
+    printf("A1 -> %ld\n", sizeof(A1));
+    printf("A2 -> %ld\n", sizeof(A2));
+    printf("A3 -> %ld\n", sizeof(A3));
+    printf("A4 -> %ld\n", sizeof(A4));
+    printf("A5 -> %ld\n", sizeof(A5));
+    printf("*A1 -> %ld\n", sizeof(*A1));
+    printf("*A2 -> %ld\n", sizeof(*A2));
+    printf("*A3 -> %ld\n", sizeof(*A3));
+    printf("*A4 -> %ld\n", sizeof(*A4));
+    printf("*A5 -> %ld\n", sizeof(*A5));
+    printf("**A1 -> %ld\n", sizeof(**A1));
+    printf("**A2 -> %ld\n", sizeof(**A2));
+    printf("**A3 -> %ld\n", sizeof(**A3));
+    printf("**A4 -> %ld\n", sizeof(**A4));
+    printf("**A5 -> %ld\n", sizeof(**A5));
     //    printf("***A1 -> %d\n", sizeof(***A1));
-    printf("***A2 -> %d\n", sizeof(***A2));
-    printf("***A3 -> %d\n", sizeof(***A3));
-    printf("***A4 -> %d\n", sizeof(***A4));
-    printf("***A5 -> %d\n", sizeof(***A5));
+    printf("***A2 -> %ld\n", sizeof(***A2));
+    printf("***A3 -> %ld\n", sizeof(***A3));
+    printf("***A4 -> %ld\n", sizeof(***A4));
+    printf("***A5 -> %ld\n", sizeof(***A5));
 }
 
 
